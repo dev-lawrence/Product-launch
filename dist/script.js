c268e54da63df87bbe2cba3bfbd001193a0078ed
dev-lawrence <lawrenceamrasakpare19@gmail.com>
2023-02-18 06:47:03 +0100

    first commit

---
+++ dist/script.js
@@ -0,0 +1,41 @@
+// Navigation menu
+const header = document.querySelector('.header');
+const toggle = document.querySelector('.toggle-menu');
+const logo = document.querySelector('.logo');
+const navMenu = document.querySelector('.nav');
+const navList = document.querySelector('.nav__list');
+const navLinks = document.querySelectorAll('.nav__link');
+const body = document.querySelector('body');
+
+// change header background color on scroll
+const showBg = () => {
+  const scroll = window.scrollY;
+
+  if (scroll > 200) {
+    header.classList.add('showBg');
+  } else {
+    header.classList.remove('showBg');
+  }
+};
+
+// Function to show navigation menu
+const showNavItems = () => {
+  navMenu.classList.toggle('showNav');
+  toggle.classList.toggle('close');
+  body.classList.toggle('hide');
+};
+
+// Function to change active link
+const changeActiveLink = (link) => {
+  link.addEventListener('click', () => {
+    const active = document.querySelector('.active');
+    active.classList.remove('active');
+    link.classList.add('active');
+    showNavItems();
+  });
+};
+
+// Event listeners
+navLinks.forEach(changeActiveLink);
+toggle.addEventListener('click', showNavItems);
+window.addEventListener('scroll', showBg);
